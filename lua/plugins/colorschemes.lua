58dffcd1948091d59aaaa70aa75e14d58bc59e63
Misha Dvali <mykhailo.dvali@gmail.com>
2024-03-10 11:22:13 +0200

    Unused colorschemes commented

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,15 +1,14 @@
 return {
-		{ "rose-pine/neovim", name = "rose-pine" },
-		{ "folke/tokyonight.nvim", lazy = false, priority = 1000, opts = {},}, 
-		{ "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = ...},
-		{ "rebelot/kanagawa.nvim", priority = 1000},
-		{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-		{ "savq/melange-nvim" },
-		{"sekke276/dark_flat.nvim",
+		-- { "rose-pine/neovim", name = "rose-pine" },
+		{ "folke/tokyonight.nvim", lazy = false, priority = 1000, opts = {},
 		config = function()
 				vim.cmd "colorscheme kanagawa-wave"
 		end
-
-},
+}, 
+		-- { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = ...},
+		{ "rebelot/kanagawa.nvim", priority = 1000},
+		-- { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+		-- { "savq/melange-nvim",},
+		-- {"sekke276/dark_flat.nvim",}
 		
 }
--- randomC#.cs
+++
@@ -1,7 +0,0 @@
-using System;
-
-public static class Program {
-		public static void main(string[] args) {
-
-		}
-}
