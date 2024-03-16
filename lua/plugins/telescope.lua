86da40f598722a10d51353a2c9e0fcd65dfd43f8
Misha Dvali <mykhailo.dvali@gmail.com>
2024-03-16 10:34:21 +0200

    Ability to close buffers with telescoope added

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,6 +4,28 @@ return {
 		tag = "0.1.5",
 		branch = "0.1.x",
 		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function() end,
+		config = function()
+			require('telescope').setup{
+				defaults = {
+					-- Default configuration for telescope goes here:
+					-- config_key = value,
+					mappings = {
+						n = {
+							['<c-d>'] = require('telescope.actions').delete_buffer,
+
+							['<leader>ff'] = ":Telescope find_files<CR>",
+							['<leader>fg'] = ":Telescope live_grep<CR>",
+							['<leader>F'] = ":Telescope buffers<CR>",
+							['<leader>fb'] = ":Telescope buffers<CR>",
+							['<leader>fh'] = ":Telescope help_tags<CR>",
+						}, -- n
+						i = {
+							["<C-h>"] = "which_key",
+							['<c-d>'] = require('telescope.actions').delete_buffer
+						} -- i
+					} -- mappings
+				}, -- defaults
+			} -- telescope setup
+		end,
 	},
 }
