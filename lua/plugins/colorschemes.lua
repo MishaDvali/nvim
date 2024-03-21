95a3280b7190e79a0071a575365eca5b22dd483f
Misha Dvali <mykhailo.dvali@gmail.com>
2024-03-21 17:33:45 +0200

    Colourscheme changed, background setted transparent

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -6,13 +6,17 @@ return {
 		priority = 1000,
 		opts = {},
 		config = function()
-			vim.cmd("colorscheme aura")
+			vim.cmd("colorscheme dark_flat")
+			vim.cmd("highlight Normal guibg=none")
+			vim.cmd("highlight NonText guibg=none")
+			vim.cmd("highlight Normal ctermbg=none")
+			vim.cmd("highlight NonText ctermbg=none")
 		end,
 	},
-	{"techtuner/aura-neovim"},
+	-- {"techtuner/aura-neovim"},
 	-- { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = ...},
 	-- { "rebelot/kanagawa.nvim", priority = 1000 },
 	-- { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 	-- { "savq/melange-nvim",},
-	{"sekke276/dark_flat.nvim",}
+	{ "sekke276/dark_flat.nvim" },
 }
