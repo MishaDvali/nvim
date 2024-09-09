46a2d26ad4d588a65a33a719210594e8d5903206
Misha Dvali <mykhailo.dvali@gmail.com>
2024-09-09 21:16:15 +0300

    Treesitter updated

--- lua/plugins/code/treesitter.lua
+++ lua/plugins/code/treesitter.lua
@@ -2,9 +2,27 @@ return {
 
 	"nvim-treesitter/nvim-treesitter",
 	config = function()
+	-- require("nvim-treesitter").setup({})
 		require("nvim-treesitter.configs").setup({
-			ensure_installed = { "python", "lua", "vim", "c_sharp"},
+			ensure_installed = {
+                "bash",
+                "c",
+                "css",
+                "html",
+                "javascript",
+                "json",
+                "lua",
+                "markdown",
+                "python",
+                "query",
+                "racket",
+                "regex",
+                "rust",
+                "vim",
+                "vimdoc",
+            },
 			auto_install = true;
 		})
+		require 'nvim-treesitter.install'.compilers = { "clang", "gcc" }
 	end,
 }
