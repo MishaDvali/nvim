4fdb430cf6f06eb6b076de1152e06e26ae63157d
Misha Dvali <mykhailo.dvali@gmail.com>
2024-09-18 13:06:26 +0300

    neoscroll activated

--- lua/plugins/visual/init.lua
+++ lua/plugins/visual/init.lua
@@ -2,5 +2,6 @@ return {
 	require("plugins.visual.colorschemes"),
 	require("plugins.visual.fineCmd"),
   require("plugins.visual.lualine"),
-  require("plugins.visual.whichkey")
+  require("plugins.visual.whichkey"),
+	require("plugins.visual.smoothScrolling")
 }
